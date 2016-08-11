7719b59440043a69dd4a37dea6d9ad36fd79e387
Andrew Sun <0xandrewsun@gmail.com>
2016-08-11 17:07:52 +0800

    Hook server process instead (blocks more methods)

--- src/com/crossbowffs/noautoshortcut/Hook.java
+++ src/com/crossbowffs/noautoshortcut/Hook.java
@@ -1,30 +1,30 @@
 package com.crossbowffs.noautoshortcut;
 
 import android.content.Intent;
-import android.util.Log;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class Hook implements IXposedHookLoadPackage {
-    private static final String TAG = "NoAutoShortcut";
     private static final String INSTALL_ACTION = "com.android.launcher.action.INSTALL_SHORTCUT";
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        final String packageName = lpparam.packageName;
-        XposedHelpers.findAndHookMethod(
-            "android.app.ContextImpl", lpparam.classLoader,
-            "sendBroadcast", Intent.class, new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    Intent intent = (Intent)param.args[0];
-                    if (intent != null && INSTALL_ACTION.equals(intent.getAction())) {
-                        Log.i(TAG, "Blocked " + packageName + " from creating shortcut");
-                        param.setResult(null);
-                    }
+        if (!"android".equals(lpparam.packageName)) {
+            return;
+        }
+
+        Class<?> cls = XposedHelpers.findClass("com.android.server.am.ActivityManagerService", lpparam.classLoader);
+        XposedBridge.hookAllMethods(cls, "broadcastIntent", new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                Intent intent = (Intent)param.args[1];
+                if (intent != null && INSTALL_ACTION.equals(intent.getAction())) {
+                    param.setResult(0);
                 }
-            });
+            }
+        });
     }
 }
